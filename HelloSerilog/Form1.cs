6aa04b1adefcae4852d03d40a1e95a55dee4f3c3
Daniel Plaisted <dsplaisted@gmail.com>
2016-05-11 22:56:46 -0700

    Create WinForms app with basic Serilog logging

---
+++ HelloSerilog/Form1.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloSerilog
+{
+    public partial class Form1 : Form
+    {
+        ILogger _log;
+
+        public Form1()
+        {
+            _log = new LoggerConfiguration()
+                .WriteTo.LiterateConsole()
+                .CreateLogger();
+
+            InitializeComponent();
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            _log.Information("Moved to ({X}, {Y})", Location.X, Location.Y);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            _log.Information("Resized to {Width} by {Height}", Width, Height);
+        }
+    }
+}
